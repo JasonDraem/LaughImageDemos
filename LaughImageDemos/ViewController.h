6d9c474a50674291cde14debef06851905097724
UncleWang <unclewang@192.168.0.101>
2016-06-23 09:45:44 +0800

    Initial Commit

---
+++ LaughImageDemos/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LaughImageDemos
+//
+//  Created by UncleWang on 16/6/23.
+//  Copyright © 2016年 UncleWang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ LaughImageDemosTests/LaughImageDemosTests.m
@@ -0,0 +1,39 @@
+//
+//  LaughImageDemosTests.m
+//  LaughImageDemosTests
+//
+//  Created by UncleWang on 16/6/23.
+//  Copyright © 2016年 UncleWang. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface LaughImageDemosTests : XCTestCase
+
+@end
+
+@implementation LaughImageDemosTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
